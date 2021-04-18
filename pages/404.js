8064325a184dc49205762cfb642d82677fd274b5
Francisco Gonzalez <francisco.gonzalez.lorca@gmail.com>
2021-04-18 19:57:16 -0400

    routing, pages, navigation, 404,basic feats

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+function NotFoundPage() {
+
+  return <div>
+    <h1>Page Not found.</h1>
+  </div>
+}
+
+export default NotFoundPage;
---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router';
+
+function ClientProjectsPage() {
+
+  const router = useRouter();
+
+  console.log(router.query);
+
+  function loadProjectHandler() {
+    // load data
+
+    // router.push('/clients/fco/projecta');
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'fco', clientprojectid: 'projecta' }
+    });
+  }
+
+  return (
+    <div>
+      <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  );
+}
+
+export default ClientProjectsPage;
